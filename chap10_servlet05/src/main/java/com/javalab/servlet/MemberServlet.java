6fa061867c3e9833fcc5b3045b181f06703eabc8
ejm8467 <ejm8467@gmail.com>
2023-07-19 18:19:39 +0900

    .

---
+++ chap10_servlet05/src/main/java/com/javalab/servlet/MemberServlet.java
@@ -0,0 +1,40 @@
+package com.javalab.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberServlet
+ */
+@WebServlet("/member")
+public class MemberServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		
+		MemberDAO dao = new MemberDAO();
+		ArrayList<MemberVO> memberList = dao.listMembers();
+		
+		// 조회 결과를 request 영역에 저장
+		request.setAttribute("memberList", memberList);
+		
+		// 프로그램 흐름을 다른 서블릿/화면으로 이동(second라는 서블릿으로 이동)
+		// 사용자에게 새로운 요청을 일으키지 않고 JSP 화면으로 직접 이동, 컨텍스트 패스
+		RequestDispatcher dispatch = request.getRequestDispatcher("memberList2.jsp");
+		dispatch.forward(request, response);
+	}
+
+
+}
---
+++ chap10_servlet05/src/main/java/com/javalab/servlet/MemberViewServlet.java
@@ -0,0 +1,55 @@
+package com.javalab.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberServlet
+ */
+@WebServlet("/view")
+public class MemberViewServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request,response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request,response);
+	}
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 1. 사용자가 입력한 한글 정보의 인코딩 처리
+		request.setCharacterEncoding("utf-8");
+		
+		// 2. 사용자에게 보낼 컨텐츠의 인코딩 처리
+		response.setContentType("text/html;charset=utf-8");
+		
+		// 3. 데이터베이스 핸들링 담당 객체 생성
+		MemberDAO dao = new MemberDAO();
+		
+		// 컨텍스트 패스
+		String contextPath = request.getContextPath();
+		
+		// 4. 파라미터 수집
+		String id = request.getParameter("id");
+		
+		// 5. 화면에서 받은 id로 사용자 조회
+		MemberVO member = dao.getMemeber(id);
+		
+		// 6. 조회한 사용자 정보를 회원 정보 보기 화면에 보여주도록 실어 보냄
+		request.setAttribute("member", member);
+		
+		// 7. 회원 정보 보기 화면으로 이동
+		RequestDispatcher dispatcher = request.getRequestDispatcher("memberView.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+
+
+}
